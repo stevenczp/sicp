7d0973c1c5953f38e88cab0dfd28395364a26a2e
cc <stevenczp@gmail.com>
2018-01-31 01:21:58 +0800

    ch1

--- ch1/1.rkt
+++ ch1/1.rkt
@@ -42,3 +42,97 @@
 
 (sqrt 4000000000000000000.0)
 
+;1.8
+(define (cube-improve y x)
+  (/ (+ (/ x (* y y)) y y) 3))
+
+(define (cube-iter guess x)
+  (if (good-enough? guess (cube-improve guess x))
+      guess
+      (cube-iter (cube-improve guess x) x)))
+
+(define (cube x)
+  (cube-iter 1.0 x))
+
+(cube 8000000000000000000000000000000)
+
+;1.10
+(define (A x y)
+  (cond ((= y 0) )
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(A 1 10);1024
+(A 2 4);65536
+(A 3 3);65536
+
+;1.11
+(define (f n)
+  (if (< n 4)
+      n
+      (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
+
+(f 10)
+
+;todo 非递归法计算f
+
+;1.12
+(define (pascal row col)
+  (cond ((= row 1) 1)
+        ((= col row) 1)
+        (else (+ (pascal (- row 1) col) (pascal (- row 1) (+ col 1))))))
+
+(pascal 5 3)
+
+;1.16 迭代快速幂
+(define (square a)
+  (* a a))
+
+(define (iter-fast-expt b n a)
+  (if (= n 0)
+      a
+      (if (even? n)
+          (iter-fast-expt (square b) (/ n 2) a)
+          (iter-fast-expt b (- n 1) (* a b)))))
+
+(define (expt b n)
+  (iter-fast-expt b n 1))
+
+(expt 2 99)
+
+;1.17 快速乘
+(define (double n)
+  (+ n n))
+
+(define (halve n)
+  (/ n 2))
+
+(define (mul x y)
+  (if (= y 1)
+      x
+      (+ x (mul x (- y 1)))))
+
+(mul 50 60)
+(define (fast-mul x y)
+  (if (= y 1)
+      x
+      (if (even? y)
+          (fast-mul (double x) (halve y))
+          (+ x (fast-mul x (- y 1))))))
+
+(fast-mul 50 60)
+
+;1.18 迭代快速乘
+(define (fast-mul-iter x y a)
+  (if (= y 0)
+      a
+      (if (even? y)
+          (fast-mul-iter (double x) (halve y) a)
+          (fast-mul-iter x (- y 1) (+ x a)))))
+
+(define (fast-multi x y)
+  (fast-mul-iter x y 0))
+
+(fast-multi 50 60)
