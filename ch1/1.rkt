baa6a60ec9ed56eb8bf9d5496e977b56b0af3e99
cc <stevenczp@gmail.com>
2018-01-31 01:21:58 +0800

    ch1

--- ch1/1.rkt
+++ ch1/1.rkt
@@ -42,3 +42,46 @@
 
 (sqrt 4000000000000000000.0)
 
+;1.8
+(define (cube-improve y x)
+  (/ (+ (/ x (* y y)) y y) 3))
+
+(define (cube-iter guess x)
+  (if (good-enough? guess (cube-improve guess x))
+      guess
+      (cube-iter (cube-improve guess x) x)))
+
+(define (cube x)
+  (cube-iter 1.0 x))
+
+(cube 8000000000000000000000000000000)
+
+;1.10
+(define (A x y)
+  (cond ((= y 0) )
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+(A 1 10);1024
+(A 2 4);65536
+(A 3 3);65536
+
+;1.11
+(define (f n)
+  (if (< n 4)
+      n
+      (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3))))))
+
+(f 10)
+
+;todo 非递归法计算f
+
+;1.12
+(define (pascal row col)
+  (cond ((= row 1) 1)
+        ((= col row) 1)
+        (else (+ (pascal (- row 1) col) (pascal (- row 1) (+ col 1))))))
+
+(pascal 5 3)
