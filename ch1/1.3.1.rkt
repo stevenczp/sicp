c5127fd48ab3437d0b996f8297aee890e11365aa
cc <stevenczp@gmail.com>
2018-02-08 23:45:40 +0800

    1.30 to 1.32

--- ch1/1.3.1.rkt
+++ ch1/1.3.1.rkt
@@ -11,6 +11,7 @@
          (sum term (next a) next b))))
 
 ;1.29
+;simpson与sum配合求积分
 (define (simpson-method f a b n)
   (let ((h (/ (- b a) n)))
     (define (y k)
@@ -27,5 +28,67 @@
       (y n)
       (sum simpson-method-term 4 add-two n)))))
 
-(simpson-method cube 0.0 1.0 2)
+(simpson-method cube 0.0 1.0 100)
 (simpson-method square 0.0 1.0 100)
+
+;1.30
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+
+(simpson-method cube 0.0 1.0 100)
+(simpson-method square 0.0 1.0 100)
+
+;1.31
+;a
+(define (product term a next b)
+  (if (> a b)
+      1
+      (* (product term (next a) next b) (term a))))
+
+(define (factorial n)
+  (define (next x)
+    (+ x 1))
+  (define (term x)
+    x)
+  (product term 1 next n))
+
+(factorial 5);120
+
+(define (pi n)
+  (define (next x)
+    (+ x 1))
+  (define (up x)
+    (+ x 1 (remainder (+ x 1) 2)))
+  (define (down x)
+    (- (+ x 2) (remainder (+ x 1) 2)))
+  (define (term x)
+    (/ (up x) (down x)))
+  (* 4.0 (product term 1 next n)))
+
+(pi 100)
+
+;b
+(define (product term a next b)
+  (define (product-iter a result)
+    (if (> a b)
+        result
+        (product-iter (next a) (* result (term a)))))
+  (product-iter a 1))
+
+;1.32
+;a
+(define (accumlate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a) (accumlate combiner null-value term (next a) next b))))
+
+(define (sum term a next b)
+  (define (term x)
+    x)
+  (define (next x)
+    (+ x 1))
+  (accumlate + 0 term a next b))
